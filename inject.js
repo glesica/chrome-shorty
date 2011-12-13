8eeab1b7d6c14e1f6ce1be63abed525c2f649cb7
George Lesica <gtlesica@eiu.edu>
2011-12-13 09:34:47 -0600

    Updated comment to show correct key.

--- inject.js
+++ inject.js
@@ -2,7 +2,7 @@ window.addEventListener('keydown', function(event) {
     // Bind to both command (for Mac) and control (for Win/Linux)
     var modifier = event.ctrlKey || event.metaKey;
 
-    // 'c' == 67
+    // 'x' == 88
     if (modifier && event.shiftKey && event.keyCode == 88) {
         // Send message to background page to toggle tab
         chrome.extension.sendRequest({shorten_url: true});
